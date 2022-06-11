9ba7b50e45017ce21aaf5e1dbb89f7670ed588f8
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-11 14:10:04 +0900

    add rspecの設定など

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/health_checks', to: 'health_checks#show'
+  root to: 'samples#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
