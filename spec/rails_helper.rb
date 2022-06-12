226943da8c0eda8c48ce1a456c1c76ee7d9c5cd1
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-12 11:33:39 +0900

    fix rpecが通らなかったので修正、その他洩らしたgemの導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Selenium::WebDriver::Chrome::Service.driver_path = '/usr/local/bin/chromedriver'
+# Selenium::WebDriver::Chrome::Service.driver_path = '/usr/local/bin/chromedriver'
