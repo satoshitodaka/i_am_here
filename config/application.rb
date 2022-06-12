60e41f57ea42fa6c03492fdcd725613960f42757
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-12 17:34:27 +0900

    add 国際化の対応

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module IAmHere
     end
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    # 翻訳ファイルへのパスはいらないっぽい？
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb, yml}').to_s]
   end
 end
