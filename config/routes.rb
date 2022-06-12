e24e0def049851c8d8b0a4bf53b93d400b92d056
todaka <75513376+satoshitodaka@users.noreply.github.com>
2022-06-12 15:20:26 +0900

    Revert "Feature/01 setting rubocop rspec ci"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/health_check', to: 'health_checks#show'
-  root to: 'samples#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
