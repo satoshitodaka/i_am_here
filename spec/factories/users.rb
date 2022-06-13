587663ad82b13655a865a7ce2f5efb6d92214276
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-13 12:42:59 +0900

    add ログイン機能の実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.unique.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    username { Faker::Name.unique.name }
   end
 end
---
+++ spec/system/logins_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "ログイン", type: :system do
+  describe 'ログイン機能' do
+    # let(:user) { create(:user) }
+    let!(:user) { create(:user) }
+    context '入力情報に誤りがある場合' do
+      it 'エラーメッセージが表示されること' do
+        visit '/login'
+        fill_in 'メールアドレス', with: 'wrong@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_on 'ログイン'
+        expect(page).to have_content 'ログインに失敗しました'
+      end
+    end
+
+    context '入力情報が正しい場合' do
+      it 'ログインできること' do
+        visit '/login'
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_on 'ログイン'
+        expect(page).to have_content 'ログインしました'
+      end
+    end
+  end
+end
