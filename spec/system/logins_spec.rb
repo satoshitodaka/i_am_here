b6c4a4e48464b56e92e1579c882e842c0977ea6f
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-17 08:22:25 +0900

    add ヘッダーの表示をログイン状態によって制御する

--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "ログイン", type: :system do
+  let(:user) { create(:user) }
   describe 'ログイン機能' do
-    # let(:user) { create(:user) }
-    let!(:user) { create(:user) }
     context '入力情報に誤りがある場合' do
       it 'エラーメッセージが表示されること' do
         visit '/login'
-        fill_in 'メールアドレス', with: 'wrong@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_on 'ログイン'
+        within '#login-form' do
+          fill_in 'メールアドレス', with: 'wrong@example.com'
+          fill_in 'パスワード', with: 'password'
+          click_on 'ログイン'
+        end
         expect(page).to have_content 'ログインに失敗しました'
       end
     end
@@ -17,10 +18,41 @@ RSpec.describe "ログイン", type: :system do
     context '入力情報が正しい場合' do
       it 'ログインできること' do
         visit '/login'
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード', with: 'password'
-        click_on 'ログイン'
+        within '#login-form' do
+          visit '/login'
+          fill_in 'メールアドレス', with: user.email
+          fill_in 'パスワード', with: 'password'
+          click_on 'ログイン'
+        end
         expect(page).to have_content 'ログインしました'
+      end
+    end
+  end
+
+  describe 'ヘッダーメニューの表示/非表示' do
+    context 'ログインしているとき' do
+      before do
+        login_as(user)
+      end
+
+      it 'メニューの表示が適切であること' do
+        visit '/'
+        within '#header' do
+          expect(page).not_to have_link 'ログイン', href: '/login'
+          expect(page).not_to have_link 'サインアップ', href: '/signup'
+          expect(page).to have_css '#header-avatar-dropdown'
+        end
+      end
+    end
+
+    context '未ログインのとき' do
+      it 'メニューの表示が適切であること' do
+        visit '/'
+        within '#header' do
+          expect(page).to have_link 'ログイン', href: '/login'
+          expect(page).to have_link 'サインアップ', href: '/signup'
+          expect(page).not_to have_css '#header-avatar-dropdown'
+        end
       end
     end
   end
